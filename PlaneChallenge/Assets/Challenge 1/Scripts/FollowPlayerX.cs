76a064e3d9f9ee93921e8c7872e04592d2d191f6
Tanishi <patelt@roverkids.org>
2023-12-04 14:17:49 -0500

    plane challenge day 2 (incomplete)

--- PlaneChallenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ PlaneChallenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -10,7 +10,7 @@ public class FollowPlayerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - plane.transform.position;
     }
 
     // Update is called once per frame
